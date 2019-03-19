5c51b9cdd1184c0feaa341d2b2fe5b1afcbdd249
radhamirah <48325591+radhamirah@users.noreply.github.com>
2019-03-19 13:28:58 +0800

    Update and rename TcpJavaServer.java to tcpserver.java

---
+++ tcpserver.java
@@ -0,0 +1,19 @@
+import.java.net.* ;
+import.java.io.* ;
+import.java.lang.* ;
+
+class TcpJavaServer 
+{
+    public static void main(String args[]) throws IOException
+    {
+        String data="Your connection has reached us \n" ;
+        ServerSocket srvr=new ServerSocket(9002) ;
+        Socket skt=srvr.accept() ;
+        System.out.pritnln("Connection has been established from ip address of : " + skt.getInetAddress()+ "\n") ;
+        PrintWriter out = new PrintWriter(skt.getOutputStream(), true) ;
+        out.print(data) ;
+        out.close() ;
+        skt.close() ;
+        srvr.close() ;
+    }
+}
